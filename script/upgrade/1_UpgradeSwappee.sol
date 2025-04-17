c7286c0d672c945fbd33ec73a7bede492d6c1e81
0xmop <matteo.marzorati94@gmail.com>
2025-04-17 15:56:31 +0200

    renaming

--- script/upgrade/1_UpgradeSwappee.sol
+++ script/upgrade/1_UpgradeSwappee.sol
@@ -6,7 +6,7 @@ import { Swappee } from "src/Swappee.sol";
 import { ISwappee } from "src/interfaces/ISwappee.sol";
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-contract DeploySwappee is Script {
+contract UpgradeSwappee is Script {
     address payable internal constant SWAPPEE_PROXY = payable(0x072099d9e6be7977fB726a2F276987224044d74e);
 
     function run() public {
